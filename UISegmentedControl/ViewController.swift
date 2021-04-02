1fc8f8a0ee7241727106331f7bdaaed28bfa5eec
devGod <devgod@mail.ru>
2021-04-02 16:35:42 +0300

    add UISlider

--- UISegmentedControl/ViewController.swift
+++ UISegmentedControl/ViewController.swift
@@ -11,16 +11,25 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        label.isHidden = true
+        slider.value = 1
+        
+        label.text = String(slider.value)
         label.font = label.font.withSize(35)
         label.textAlignment = .center
         label.numberOfLines = 2
         
         segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .blue
     }
 
     @IBAction func choiceSegment(_ sender: UISegmentedControl) {
@@ -40,6 +49,13 @@ class ViewController: UIViewController {
         default:
             print("Sothing wrong!")
         }
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        label.text = String(sender.value)
+        
+        let backgrandColor = self.view.backgroundColor
+        self.view.backgroundColor = backgrandColor?.withAlphaComponent(CGFloat(sender.value))
     }
     
 }
